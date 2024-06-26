c4e9e65322ce428b1446524e7e8d1a40d3c3c54e
Zeista01 <150901226+Zeista01@users.noreply.github.com>
2024-06-26 12:49:03 +0530

    Delete scroll.js

--- scroll.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function() {
-    var dropdownContent = document.querySelector(".dropdown-content");
-    if (window.scrollY === 0) {
-        dropdownContent.classList.add("show");
-    } else {
-        dropdownContent.classList.remove("show");
-    }
-});
